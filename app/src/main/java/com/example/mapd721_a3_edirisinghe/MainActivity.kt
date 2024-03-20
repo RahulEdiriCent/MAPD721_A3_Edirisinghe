a9ce5095520822bb9ad602e6c7c5d940d7179ce1
Rahul Edirisinghe <rahulediri@gmail.com>
2024-03-20 19:49:59 -0400

    Forgot Git Repository, Redoing Project step by step -> Removing Greeting Function and adding gradle implementations: naviagtion and shapes

--- app/src/main/java/com/example/mapd721_a3_edirisinghe/MainActivity.kt
+++ app/src/main/java/com/example/mapd721_a3_edirisinghe/MainActivity.kt
@@ -22,25 +22,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MAPD721_A3_EdirisingheTheme {
-        Greeting("Android")
+        //Greeting("Android")
     }
 }
